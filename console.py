1321918b936d87d117ff4934ae4bf190cbcc0528
root <richardchekwas@gmail.com>
2023-07-16 18:13:58 -0700

    aa

--- console.py
+++ console.py
@@ -113,6 +113,13 @@ class HBNBCommand(cmd.Cmd):
         if found is False:
             print("** class doesn't exist **")
 
+    def default(self, line):
+        """Handle defaults stuff."""
+
+        d_line = line.split(".")
+        if d_line[1] == "all()":
+            self.do_all(d_line[0])
+
     def do_update(self, line):
         """ Updates an instance based on the class name and id by adding
         or updating attribute (save the change into the JSON file)"""
